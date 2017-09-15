82db36d0c89de47675e3ee90b2683366e839bcf8
raybchoi <raybchoi@gmail.com>
2017-09-14 17:19:42 -0700

    create and add routes

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -6,10 +6,24 @@ class AlbumsController < ApplicationController
     render :index
   end
 
-
-
   def show
     @album = Album.find(params[:id])
     render :show
+  end
+
+  def new
+    @album = Album.new
+    render :new
+  end
+
+  def create
+    Album.create (album_params)
+    redirect_to('/albums')
+  end
+
+  private
+  def album_params
+    # this is known as strong parameters, and is done for security purposes
+    params.require(:album).permit(:title, :artist, :year, :cover_art, :song_count)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # when you go to localhost:3000/albums then go to the to route album controller and then find the index method which will then tell you to go the the correct view folder
+
+
+  # as: 'albums' is the keywords for the path when creating links
   get "/albums" => "albums#index", as: 'albums'
-  # get "/albums/new" => "albums#new", as: 'new_album'
+
+
+  get "/albums/new" => "albums#new", as: 'new_album'
+
   # this is routes for the specific ID
   get "/albums/:id" => "albums#show", as: 'album'
-  # post "/albums" => "albums#create"
+
+  post "/albums" => "albums#create"
 
 end
